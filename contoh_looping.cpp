263228968e372ea37d2c0361821a5558b8ead623
RafiiArifNugroho <rafialamcitra@gmail.com>
2025-03-17 11:45:17 +0700

    pencacah turun

--- contoh_looping.cpp
+++ contoh_looping.cpp
@@ -18,3 +18,12 @@ for (n = 0; n < 5; n++)
 }
 cout << " Nilai terakhir = " << n << endl;
 cout << endl;
+
+cout << " perulangan for pencacah turun" << endl;
+// perulangan dengan for
+for (n = 5; n > 0; n--)
+    (
+        cout << " nilai n = " << n << "Selamat Datang " << endl;)
+            cout
+        << " Nilai terakhir = " << n;
+cout << endl;
