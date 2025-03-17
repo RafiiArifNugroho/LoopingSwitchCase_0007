c24bae493abfe98b021bafd525c1b6cb2644506f
RafiiArifNugroho <rafialamcitra@gmail.com>
2025-03-17 12:24:45 +0700

    membuat switch case

--- Contoh_switchCase.cpp
+++ Contoh_switchCase.cpp
@@ -15,4 +15,38 @@ int main()
     int pilihan;
     float panjang, lebar;
     float jejari;
+
+    do
+    {
+        cout << "Menu" << endl;
+        cout << "1. Luas Persegi Panjang" << endl;
+        cout << "2. Luas Lingkaran" << endl;
+        cout << "3. Keluar" << endl;
+
+        cout << " Pilihan (1/2/3) = ";
+        cin >> pilihan;
+
+        switch (pilihan)
+        {
+        case 1:
+            cout << "Hitung luas persegi panjang" << endl;
+            cout << "Masukan panjang = ";
+            cin >> panjang;
+            cout << "Masukan lebar = ";
+            cin >> lebar;
+            cout << "Luas persegi panjang = " << luasPersegiPanjang(panjang, lebar) << endl;
+            break;
+        case 2:
+            cout << "Hitung luas lingkaran" << endl;
+            cout << "Masukan jari-jari = ";
+            cin >> jejari;
+            cout << "Luas lingkaran = " << luasLingkaran(jejari) << endl;
+            break;
+        case 3:
+            break;
+        default:
+            cout << "Pilihan salah, pilihan harus di antara 1-3" << endl;
+            break;
+        }
+    } while (pilihan != 3);
 }
