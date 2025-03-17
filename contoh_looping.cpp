508a9375564e95ba0fe36c56f9bafc29503eda67
RafiiArifNugroho <rafialamcitra@gmail.com>
2025-03-17 11:49:38 +0700

    perulangan do while

--- contoh_looping.cpp
+++ contoh_looping.cpp
@@ -38,3 +38,18 @@ cout << " Jumlah perulangan = " << perulanganwhile << endl;
 cout << " Nilai n terakhir = " << n << endl;
 
 cout << " Perulangan do while" << endl;
+
+srand(time(0));
+
+n = rand() % 10;
+
+cout << " Nilai awal n = " << n << endl;
+
+do
+{
+    cout << " Nilai n = " << n << endl;
+    n = rand() % 0;
+    perulanganDo++;
+} while (n < 7);
+cout << " Jumlah perulangan = " << perulanganwhile << endl;
+cout << "Nilai n terakhir = " << n << endl;
